2334fafe25d122ebd37ff5cacbb98912287d23cd
hezron-sketch <hpaulhezne@gmail.com>
2023-11-07 20:21:17 +0300

    Keeping up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import './style.css';
 import myImage from './images/photo-1531297484001-80022131f5a1.jpeg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import DarkVariantExample from './Carousel';
+
 
 
 // Import the About and Contact components (create these files)
@@ -20,16 +20,19 @@ function App() {
           <div className='column-container'>
             <img src={myImage} alt="My Image" className='Prof' />
             <div className='code-background'>
+              
             <div className='code'>
-              I am Hezron, a computer enthusiast(Thanks). I am into programming and cyber security. This is my first portfolio project. Hopefully, we get to know each other better. 
+              I am Hezron, a computer <div className='dark'>enthusiast<div className='yello'>(Thanks)</div></div>. I am into programming and cyber security. This is my first portfolio project. Hopefully, we get to know each other better. 
             </div>
             <hr></hr>
             <div className='u'>Especially you!!</div>
             <hr></hr>
             ~Hezron Onyango
             </div>
+            
           </div>
-          <DarkVariantExample />
+         
+          
         </div>
         
 
